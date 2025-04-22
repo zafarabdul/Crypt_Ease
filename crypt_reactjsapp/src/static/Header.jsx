c3d7cf182f9ce0739177baaeb3cbcc41bafe6133
zafarabdul <zafarabdul05@gmail.com>
2025-04-22 22:48:44 +0530

    header1

--- crypt_reactjsapp/src/static/Header.jsx
+++ crypt_reactjsapp/src/static/Header.jsx
@@ -1,4 +1,17 @@
 function Header(){
-    return <div><h1>Making</h1></div>
+    const msct = {
+        fontFamily: "Poppins, sans-serif",
+        fontWeight: 800,
+        fontStyle: "normal",
+        fontSize: "20px",
+    };
+    return <>
+        <div>
+            <div className="cryptease"  style={msct}>
+                <p>CryptEase</p>
+            </div>
+            <div>Buy a Coffee</div>
+        </div>
+    </>
 }
 export default Header
