7b9dc26f1d9d62b1e3670b338155f087861b3fb8
zafarabdul <zafarabdul05@gmail.com>
2025-05-04 14:31:22 +0530

    .adsense

--- crypt_reactjsapp/src/static/Footer.jsx
+++ crypt_reactjsapp/src/static/Footer.jsx
@@ -1,11 +1,31 @@
 import '../App.css';
-import Lor from './Lor.jsx';
 export default function Footer(){
+    const z1={
+        display:"flex",
+        gap:"16px",
+        fontSize:"larger",
+        backgroundColor:" rgb(26, 26, 26)",
+    }
     return (
-        <>
-            <h1>Footer Loding</h1>
-            <Lor/>
-            <Lor/>
-        </>
+        <footer>
+            <div>
+                <h3>Quick Links</h3>
+                <ul>
+                    <li>Encrypt</li>
+                    <li>Decrypt</li>
+                    <li>Purchase Id</li>
+                    <li>Contact Me</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Social Media</h3>
+                <div style={z1}>
+                <p><a href="https://github.com/zafarabdul"><i class="fa-brands fa-github"></i></a></p>
+                <p><i class="fa-brands fa-instagram"></i></p>
+                <p><i class="fa-brands fa-facebook"></i></p>
+                <p><i class="fa-brands fa-x-twitter"></i></p>
+                </div>
+            </div>
+        </footer>
     )
 }
