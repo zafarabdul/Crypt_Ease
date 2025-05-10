8c00ca3c2a7c267b490c79314de871011c639acc
chaimass6 <chaitu.6914@gmail.com>
2025-05-10 17:25:05 +0530

    ok

--- crypt_reactjsapp/src/static/Footer.jsx
+++ crypt_reactjsapp/src/static/Footer.jsx
@@ -6,6 +6,7 @@ export default function Footer(){
         fontSize:"larger",
         backgroundColor:" rgb(26, 26, 26)",
     }
+    
     const col={
         backgroundColor:" rgb(26, 26, 26)",
     }
