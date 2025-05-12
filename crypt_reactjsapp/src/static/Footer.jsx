5fead043337ea02020dab6ef807af2764aa5a51a
chaimass6 <chaitu.6914@gmail.com>
2025-05-12 23:52:44 +0530

    ok

--- crypt_reactjsapp/src/static/Footer.jsx
+++ crypt_reactjsapp/src/static/Footer.jsx
@@ -79,10 +79,18 @@ export default function Footer() {
             <div style={sectionStyle}>
                 <h3 style={{ ...gradientText, ...titleStyle }}>Social Media</h3>
                 <div style={iconStyle}>
-                    <a href="https://github.com/zafarabdul" style={{ color: '#ccc' }}><FaGithub /></a>
-                    <a href="#" style={{ color: '#ccc' }}><FaInstagram /></a>
-                    <a href="#" style={{ color: '#ccc' }}><FaFacebook /></a>
-                    <a href="#" style={{ color: '#ccc' }}><FaXTwitter /></a>
+                    <a href="https://github.com/zafarabdul" style={{ color: '#ccc' }}>
+                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" style={{ width: '24px', height: '24px' }} />
+                    </a>
+                    <a href="#" style={{ color: '#ccc' }}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram" style={{ width: '24px', height: '24px' }} />
+                    </a>
+                    <a href="#" style={{ color: '#ccc' }}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" style={{ width: '24px', height: '24px' }} />
+                    </a>
+                    <a href="#" style={{ color: '#ccc' }}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/6/60/Twitter_Logo_as_of_2021.svg" alt="Twitter" style={{ width: '24px', height: '24px' }} />
+                    </a>
                 </div>
             </div>
         </footer>
