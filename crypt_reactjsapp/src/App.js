30dcffbdd78b228b30e5f2d3edbbb47dc76cca2d
chaimass6 <chaitu.6914@gmail.com>
2025-05-12 23:41:16 +0530

    ok

--- crypt_reactjsapp/src/App.js
+++ crypt_reactjsapp/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Footer from './static/Footer.jsx';
 import Header from './static/Header.jsx';
 import Section from './static/Section.jsx';
-
 function App() {
   return (
     <div>
