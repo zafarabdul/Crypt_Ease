8afe3eed903d0efb6ba8654835d3ea48854e8067
chaimass6 <chaitu.6914@gmail.com>
2025-05-13 11:48:56 +0530

    ok

--- crypt_reactjsapp/src/App.js
+++ crypt_reactjsapp/src/App.js
@@ -3,11 +3,11 @@ import Footer from './static/Footer.jsx';
 import Header from './static/Header.jsx';
 import Section from './static/Section.jsx';
 function App() {
-  // const appStyle = {
-  //   backgroundColor: "#000",
-  //   color: "#fff",
-  //   fontFamily: "'Roboto', sans-serif",
-  //   minHeight: "100vh",
+  const appStyle = {
+    backgroundColor: "#000",
+    color: "#fff",
+    fontFamily: "'Roboto', sans-serif",
+    minHeight: "100vh",
   };
   return (
     <div>
