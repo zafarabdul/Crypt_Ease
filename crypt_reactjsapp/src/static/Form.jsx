6cb5e9546f59483686b380e43259dc904318c6d1
zafarabdul <zafarabdul05@gmail.com>
2025-06-20 20:50:37 +0530

    change div completed

--- crypt_reactjsapp/src/static/Form.jsx
+++ crypt_reactjsapp/src/static/Form.jsx
@@ -1,16 +1,16 @@
 import '../App.css';
-export default function Form({head}){
+export default function Form(props){
         const del={
                 height:"90px",
                 width:"40vw",
         }
         return(
                 <form>
-                        <h1>Encrypt Data</h1>
-                        <p className="formEle">Id : <input type='text' required/></p>
-                        <p className="formEle">Secret Number : <input type='text' required/></p>
+                        <h1>{props.head}</h1>
+                        <p className="formEle">{props.first} : <input type='text' required/></p>
+                        <p className="formEle">{props.second} : <input type='text' required/></p>
                         <p className="formEle">Message : <textarea style={del} type='textbox' required/></p>
-                        <button type="submit" id="Submit">SAVE MESSAGE</button>
+                        <button type="submit" id="Submit">{props.message}</button>
                 </form>
         )
 }
--- crypt_reactjsapp/src/static/Section_1.jsx
+++ crypt_reactjsapp/src/static/Section_1.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import '../App.css';
 import Form from './Form';
 export default function Section_1() {
@@ -5,15 +6,17 @@ export default function Section_1() {
     const fst={
         backgroundColor:"black",
     }
+    const [count,setCount] = useState(0);
+
     return (
         <div id="main">
             <div id="main_1">
-                <Form head="Encrypt Data" style={fst} />
-                <img src="/boy_1.png" alt="Lock Icon" height={size} width={400}/>
+                {count %2 ==0 ? <Form head="Decrypt Data" message="GET MESSAGE" first="Id" second="Secret Number" style={fst}/> : <Form head="Encrypt Data" message="SAVE MESSAGE" first="Id" second="Secret Number"  style={fst}/> }
+                {count %2 ==0 ? <img src="/boy_1.png" alt="Lock Icon" height={size} width={400}/>:<img src="/girl_1.png" alt="Lock Icon" height={size} width={360}/>}
             </div>
-            <div id="section2">
+            <div id="section2" onClick={()=> setCount(count +1)}>
                 <h>
-                <h1>Decrypt Data</h1>
+                {count %2 == 0 ? <h1>Encrypt Data</h1> : <h1>Decrypt Data</h1> }
                 <p>Save Your Message With High Security</p>
                 </h>
             </div>
