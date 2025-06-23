c6242c199643b67d02a2813cbc61e098a600f03f
zafarabdul <zafarabdul05@gmail.com>
2025-06-23 19:06:02 +0530

    dep1

--- Crypt_Ease/settings.py
+++ Crypt_Ease/settings.py
@@ -43,19 +43,21 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://crypt-ease.vercel.app",
 ]
 ROOT_URLCONF = 'Crypt_Ease.urls'
 
--- djApp/models.py
+++ djApp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Data(models.Model):
     id = models.CharField(max_length=12,primary_key=True)
     secKey = models.IntegerField()
-    eMessage = models.CharField()
+    eMessage = models.CharField(max_length=1000)
