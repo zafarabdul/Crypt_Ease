be1d9280ad787f4efb933e4934636887102be901
zafarabdul <zafarabdul05@gmail.com>
2025-06-23 19:59:27 +0530

    .

--- crypt_reactjsapp/src/static/Form.jsx
+++ crypt_reactjsapp/src/static/Form.jsx
@@ -35,7 +35,7 @@ const handleSubmit = async (e) => {
   }
 
   try {
-    const response = await fetch('http://127.0.0.1:8000/api/', {
+    const response = await fetch('https://crypt-ease.onrender.com/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
