8fe8167b7be0b57bcd741a72b4c1a2e42f29be02
zafarabdul <zafarabdul05@gmail.com>
2025-07-03 23:49:11 +0530

    nav changed

--- crypt_reactjsapp/src/static/Header.jsx
+++ crypt_reactjsapp/src/static/Header.jsx
@@ -10,6 +10,7 @@ function Header(){
         padding:'20px 10vw',
         zIndex:"10",
         margin:"-10px -10px",
+        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
     };
 
     const cryptease = {
@@ -20,7 +21,7 @@ function Header(){
 
 return (
     <>
-    <nav style={{...nav, boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)"}}>
+    <nav style={nav}>
         <div style={cryptease}>
             <h1 className='gradient-text'>
                 Crypt Ease
